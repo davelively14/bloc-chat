c603786ec0fa2f69853949df8a6b7949f248d9e7
Dave Lively <davelively14@gmail.com>
2017-01-30 13:31:44 -0500

    Added settings modal

--- app/scripts/controllers/ModalSettingsCtrl.js
+++ app/scripts/controllers/ModalSettingsCtrl.js
@@ -1,27 +1,33 @@
 (function() {
   function ModalSettingsCtrl($scope, $uibModalInstance, roomId, Chat, User) {
+    var currentUser = User.getCurrentUser()
     var usersTemp = User.all();
     var roomTemp = Chat.getObj(roomId);
 
-    usersTemp.$loaded().then(function() {
-      roomTemp.$loaded().then(function() {
+    currentUser.$loaded().then(function() {
+      usersTemp.$loaded().then(function() {
+        roomTemp.$loaded().then(function() {
 
-        for (var userIndex = 0; userIndex < usersTemp.length; userIndex++) {
-          for (var rmUsrIndx = 0; rmUsrIndx < roomTemp.users.length; rmUsrIndx++) {
-            if (roomTemp.users[rmUsrIndx] === usersTemp[userIndex].uid) {
-              usersTemp[userIndex].member = true;
+          for (var userIndex = 0; userIndex < usersTemp.length; userIndex++) {
+            for (var rmUsrIndx = 0; rmUsrIndx < roomTemp.users.length; rmUsrIndx++) {
+              if (roomTemp.users[rmUsrIndx] === usersTemp[userIndex].uid) {
+                usersTemp[userIndex].member = true;
+                if (usersTemp[userIndex].uid === currentUser.uid) {
+                  usersTemp[userIndex].owner = true;
+                }
+              }
+            }
+
+            if (!usersTemp[userIndex].member) {
+              usersTemp[userIndex].member = false;
             }
           }
 
-          if (!usersTemp[userIndex].member) {
-            usersTemp[userIndex].member = false;
-          }
-        }
-
-        $scope.room = roomTemp;
-        $scope.users = usersTemp;
+          $scope.room = roomTemp;
+          $scope.users = usersTemp;
+        });
       });
-    });
+    })
 
     $scope.cancel = function() {
       $uibModalInstance.close();
